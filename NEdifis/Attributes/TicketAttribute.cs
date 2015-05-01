ab6835a0140804d3d0302d1bb38ea9793cfc7381
Thomas Mentzel <thomas.mentzel@softwarekueche.de>
2015-05-01 09:20:19 +0200

    ticket attribute added so you can add a ticket to your test

---
+++ NEdifis/Attributes/TicketAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NEdifis.Attributes
+{
+    [TestedBy(typeof(TicketAttribute_Should))]
+    public class TicketAttribute : Attribute
+    {
+        public string Reference { get; private set; }
+        public int? Id { get; private set; }
+
+        public TicketAttribute(string reference) : this(null, reference) { }
+
+        public TicketAttribute(int id) : this(id, null) { }
+
+        public TicketAttribute(int? id, string reference)
+        {
+            Id = id;
+            Reference = reference;
+        }
+    }
+}
