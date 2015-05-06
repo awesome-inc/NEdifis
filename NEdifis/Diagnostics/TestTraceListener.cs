137f10b320b35da7660689f16e47d652035cc7dc
Thomas Mentzel <thomas.mentzel@softwarekueche.de>
2015-05-06 20:55:41 +0200

    test trace listener

---
+++ NEdifis/Diagnostics/TestTraceListener.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using NEdifis.Attributes;
+
+namespace NEdifis.Diagnostics
+{
+    [TestedBy(typeof(TestTraceListener_Should))]
+    sealed class TestTraceListener : TraceListener
+    {
+        #region construct and dispose
+
+        public TestTraceListener()
+        {
+            // Set default values
+            Name = "Test Trace Listener (NEdifis)";
+            ActiveTraceLevel = TraceLevel.Info;
+
+            // Subscribe to all trace
+            Trace.Listeners.Add(this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Trace.Listeners.Remove(this);
+            base.Dispose(disposing);
+        }
+
+        #endregion
+
+        public TraceLevel ActiveTraceLevel { get; set; }
+
+        private readonly Dictionary<int, List<string>> _levelMessages = new Dictionary<int, List<string>>
+            {
+                {(int)TraceLevel.Error, new List<string>()},
+                {(int)TraceLevel.Warning, new List<string>()},
+                {(int)TraceLevel.Info, new List<string>()},
+                {(int)TraceLevel.Verbose, new List<string>()}
+            };
+
+        public IEnumerable<string> MessagesFor(TraceLevel level)
+        {
+            return _levelMessages[(int)level].AsReadOnly();
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            // Check the type
+            switch (eventType)
+            {
+                case TraceEventType.Critical:
+                case TraceEventType.Error:
+                    if ((ActiveTraceLevel == TraceLevel.Error) ||
+                        (ActiveTraceLevel == TraceLevel.Warning) ||
+                        (ActiveTraceLevel == TraceLevel.Info) ||
+                        (ActiveTraceLevel == TraceLevel.Verbose))
+                    {
+                        _levelMessages[(int)TraceLevel.Error].Add(message);
+                    }
+                    break;
+
+                case TraceEventType.Warning:
+                    if ((ActiveTraceLevel == TraceLevel.Warning) ||
+                        (ActiveTraceLevel == TraceLevel.Info) ||
+                        (ActiveTraceLevel == TraceLevel.Verbose))
+                    {
+                        _levelMessages[(int)TraceLevel.Warning].Add(message);
+                    }
+                    break;
+
+                case TraceEventType.Information:
+                    if ((ActiveTraceLevel == TraceLevel.Info) ||
+                        (ActiveTraceLevel == TraceLevel.Verbose))
+                    {
+                        _levelMessages[(int)TraceLevel.Info].Add(message);
+                    }
+                    break;
+
+                default:
+                    if (ActiveTraceLevel == TraceLevel.Verbose)
+                    {
+                        _levelMessages[(int)TraceLevel.Verbose].Add(message);
+                    }
+                    break;
+            }
+        }
+
+        public override void TraceEvent(
+            TraceEventCache eventCache,
+            string source,
+            TraceEventType eventType,
+            int id,
+            string format,
+            params object[] args)
+        {
+            TraceEvent(eventCache, source, eventType, id, string.Format(format, args));
+        }
+
+        public override void Write(string message)
+        {
+            WriteLine(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            // Invoke the event (but only when output is set to verbose)
+            if (ActiveTraceLevel == TraceLevel.Verbose)
+            {
+                _levelMessages[(int)TraceLevel.Verbose].Add(message);
+            }
+        }
+    }
+}
---
+++ NEdifis/Diagnostics/TestTraceListener_Should.cs
@@ -0,0 +1,60 @@
+using System.Diagnostics;
+using FluentAssertions;
+using NEdifis.Attributes;
+using NUnit.Framework;
+
+namespace NEdifis.Diagnostics
+{
+    [TestFixtureFor(typeof(TestTraceListener))]
+    // ReSharper disable once InconsistentNaming
+    class TestTraceListener_Should
+    {
+        [Test]
+        public void Be_Creatable()
+        {
+            // lets count the before
+            var listenerCount = Trace.Listeners.Count;
+
+            using (var ttl = new TestTraceListener())
+            {
+                Trace.Listeners.Count.Should().Be(listenerCount + 1);
+
+                Trace.TraceError("here is a message");
+                ttl.MessagesFor(TraceLevel.Error).Should().Contain("here is a message");
+                ttl.MessagesFor(TraceLevel.Info).Should().NotContain("here is a message");
+            }
+
+            // see if it got deregistered
+            Trace.Listeners.Count.Should().Be(listenerCount);
+        }
+
+        [Test]
+        public void Handle_Error_Warn_Information()
+        {
+            using (var ttl = new TestTraceListener())
+            {
+                Trace.TraceError("cool error");
+                Trace.TraceWarning("awesome warning");
+                Trace.TraceInformation("great information");
+                Trace.WriteLine("nice debug");
+
+                ttl.MessagesFor(TraceLevel.Error).Should().Contain("cool error");
+                ttl.MessagesFor(TraceLevel.Warning).Should().Contain("awesome warning");
+                ttl.MessagesFor(TraceLevel.Info).Should().Contain("great information");
+                ttl.MessagesFor(TraceLevel.Verbose).Should().BeEmpty(because: "level is set to info by default");
+            }
+        }
+
+        [Test]
+        public void Handle_Debug()
+        {
+            using (var ttl = new TestTraceListener())
+            {
+                ttl.ActiveTraceLevel=TraceLevel.Verbose;
+                Trace.WriteLine("nice debug");
+
+                ttl.MessagesFor(TraceLevel.Verbose).Should().Contain("nice debug");
+            }
+        }
+    }
+}
